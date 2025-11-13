62067a6bc10ea41f45fe141c19ac958438ecf935
Brendan Graham <brendanvgraham@gmail.com>
2025-11-13 10:35:41 -0500

    remove dead code

--- Code/UI/tailwind.config.js
+++
@@ -1,6 +0,0 @@
-﻿export default {
-  darkMode: 'class',
-  content: ['./index.html','./src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
-  plugins: [],
-}
--- Code/get_embedding_function.py
+++
@@ -1,5 +0,0 @@
-from langchain_ollama import OllamaEmbeddings
-
-def get_embedding_function(model_name: str = "llama3.1"):
-    """Return embedding function for a given Ollama model."""
-    return OllamaEmbeddings(model=model_name)
